44dbd0c5853d1b2a0c5ded8dacddc8cee2a15c97
Andrea Berardi <andrea.berardi@verbinteractive.com>
2026-01-15 01:01:17 +0100

    Adding djangoapi app, starting working with django-ninja

---
+++ djangoapi/djangoapi/api.py
@@ -0,0 +1,73 @@
+from ninja import NinjaAPI, Schema
+from typing import List
+from dashboard.models import Customer, Invoice
+api = NinjaAPI()
+
+# Add
+@api.get("/add")
+def add(request, a: int, b: int):
+    return {"result": a + b}
+
+# Simple Hello
+@api.get("/hello")
+def hello(request, name: str = ''):
+  if name == '':
+    return {"response": "No name provided to say hello"}
+  else:
+    return {"response": f"Hello {name}"}
+
+# Hello using path for params
+@api.get("/hello-from-path/{name}")
+def helloFromPath(request, name):
+  return {"response": f"Hello {name}"}
+
+# Hello reading params from body
+class HelloBodySchema(Schema):
+  name: str = 'Berry'
+
+@api.post("/hello-from-body")
+def helloFromBody(request, data: HelloBodySchema):
+  return f"Responding from helloFromBody: Hey {data.name}!"
+
+# Blog index api
+@api.get("/blog")
+def blogList(request):
+    return {"data": "Blog post list here"}
+
+
+# Current user info
+class UserInfoSchema(Schema):
+  username: str
+  is_authenticated: bool
+  # Unauthenticated users don't have the following fields, so provide defaults.
+  email: str = None
+  first_name: str = None
+  last_name: str = None
+
+@api.get("/user/info", response=UserInfoSchema)
+def userInfo(request):
+  return request.user
+
+# Dashboard API
+# Customers list
+class CustomerSchemaOut(Schema):
+    name: str
+    email: str
+
+@api.get('/customers', response=List[CustomerSchemaOut])
+def customersList(request):
+  customersQs = Customer.objects.all()
+  return customersQs
+
+
+# Dashboard API
+# Invoices list
+class InvoiceSchemaOut(Schema):
+    customer: CustomerSchemaOut
+    amount: float
+    status: str
+
+@api.get('/invoices', response=List[InvoiceSchemaOut])
+def invoicesList(request):
+  invoicesQs = Invoice.objects.all()
+  return invoicesQs
